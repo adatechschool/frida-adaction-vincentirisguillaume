3e1a5f281ffc1c7cb51ed436be0f5a3b99e03bff
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-10-08 16:30:12 +0200

    ADD: CSS Index

--- front/scriptsjs/assos.js
+++ front/scriptsjs/assos.js
@@ -17,7 +17,7 @@ const getVolunteer = async () => {
                 <td>${volunteer.points}</td>
                 <td>${volunteer.location}</td>
                 <td>${volunteer.email}</td>
-                <td><button class="remove-btn">supprimer</button></td>
+                <td><button class="remove-btn">X</button></td>
             `;
             userLine.appendChild(row);
             console.log("volunteer.id:", volunteer.id)
@@ -73,7 +73,7 @@ const getVolunteersByAssociation = async (associationName) => {
                 <td>${volunteer.points}</td>
                 <td>${volunteer.location}</td>
                 <td>${volunteer.email}</td>
-                <td><button class="remove-btn">supprimer</button></td>
+                <td><button class="remove-btn">X</button></td>
             `;
             userLine.appendChild(row);
         });
